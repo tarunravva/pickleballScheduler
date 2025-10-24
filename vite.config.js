34da6c41a12248b92311e4831b6d08ba9418d7a9
tarunravva <tarunravva@gmail.com>
2025-10-24 03:04:56 +0000

    minor updates to build app

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/pickleballScheduler/", // <-- required for GitHub Pages
   plugins: [react()]
 });
